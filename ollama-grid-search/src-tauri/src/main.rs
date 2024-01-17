ad75b7c01d17c9b127c8187ca5c844890f511fd3
dezoito <an.dezoito@gmail.com>
2024-01-17 17:36:11 -0300

    Removed default styles

--- ollama-grid-search/src-tauri/src/main.rs
+++ ollama-grid-search/src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from Rust!...", name)
 }
 
 fn main() {
--- ollama-grid-search/src/App.tsx
+++ ollama-grid-search/src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -14,22 +12,6 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
       <form
         className="row"
         onSubmit={(e) => {
