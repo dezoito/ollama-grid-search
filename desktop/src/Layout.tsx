6fb22c5f0139b17a5dab6e400cf29b64e629357a
Dezoito <an.dezoito@gmail.com>
2024-01-23 15:55:31 -0300

    Created component to display errors

--- desktop/src/Layout.tsx
+++ desktop/src/Layout.tsx
@@ -1,19 +1,9 @@
-import { invoke } from "@tauri-apps/api/tauri";
-
-import { useQuery } from "@tanstack/react-query";
+import ModelSelector from "./components/filters/ModelSelector";
 import { ModeToggle } from "./components/mode-toggle";
 
 function Layout() {
   // const [name, setName] = useState("");
   // const [models, setModels] = useState<string[]>([]);
-
-  async function get_models() {
-    const models = await invoke("get_models");
-    console.log("type of models", typeof models);
-    return models;
-  }
-
-  const query = useQuery({ queryKey: ["get_models"], queryFn: get_models });
 
   return (
     <div className="flex flex-col h-screen">
@@ -30,24 +20,15 @@ function Layout() {
       </header>
       <div className="flex flex-1 overflow-hidden">
         {/* sidebar */}
-        <div className="w-96 border-r border-gray-200 dark:border-gray-800">
-          <nav className="flex flex-col gap-6 p-4">
-            {query.error && <>{query.error}</>}
-            {query.isFetched && (
-              <div className="bg-red-400">
-                {(query.data as string[]).map((model: string) => (
-                  <div>{model}</div>
-                ))}
-              </div>
-            )}
-          </nav>
+        <div className="w-96 border-r border-gray-200 dark:border-gray-800 gap-6 p-4">
+          <ModelSelector />
         </div>
 
         {/* main div */}
         <main className="flex-1 p-4 overflow-y-auto">
           <div className="p-2 m-1">
             <p className="text-gray-500 dark:text-gray-400">
-              Experiment results
+              Instructions and Experiment results
             </p>
           </div>
         </main>
---
+++ desktop/src/components/filters/ModelSelector.tsx
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api/tauri";
+import AlertError from "../ui/AlertError";
+
+export default function ModelSelector() {
+  // const [name, setName] = useState("");
+  // const [models, setModels] = useState<string[]>([]);
+
+  async function get_models() {
+    const models = await invoke("get_models");
+    return models;
+  }
+
+  const query = useQuery({ queryKey: ["get_models"], queryFn: get_models });
+
+  if (query.isError) {
+    return (
+      <div>
+        <AlertError message={JSON.stringify(query.error)} />
+      </div>
+    );
+  }
+
+  if (query.isLoading) {
+    return <p>Loading.</p>;
+  }
+
+  return (
+    <div>
+      {query.isFetched && (
+        <div className="bg-red-400">
+          {(query.data as string[]).map((model: string) => (
+            <div>{model}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
