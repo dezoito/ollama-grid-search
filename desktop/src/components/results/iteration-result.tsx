e306a42f83c0ed270a8e23736ca7b4369540aac2
Dezoito <an.dezoito@gmail.com>
2024-02-08 18:45:10 -0300

    UI: Collapsible display with inference paramters

--- desktop/src/components/results/iteration-result.tsx
+++ desktop/src/components/results/iteration-result.tsx
@@ -1,7 +1,11 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 // import { invoke } from "@tauri-apps/api/tauri";
 import { TParamIteration } from "@/Interfaces";
-import { cn } from "@/lib/utils";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { get_inference } from "../queries";
 import { Button } from "../ui/button";
@@ -13,6 +17,7 @@ interface IProps {
 
 export default function IterationResult(props: IProps) {
   const { params } = props;
+  const { prompt, model, temperature, repeat_penalty, top_k, top_p } = params;
   const queryClient = useQueryClient();
 
   // Use only the cached queries from the parent component
@@ -21,21 +26,39 @@ export default function IterationResult(props: IProps) {
     queryFn: () => get_inference(params),
     enabled: true,
     staleTime: Infinity,
-    cacheTime: Infinity,
+    // cacheTime: Infinity,
   });
 
   if (query.isFetching) {
-    return <>Generating...</>;
+    return <div className="my-3 p-4 ">Generating...</div>;
   }
   return (
-    <div>
+    <div className="flex flex-row">
       {/* <div>{prompt}</div> */}
       {/* <div>{JSON.stringify(params)}</div> */}
-      <div className="bg-slate-700 m-3 opacity-50">
+      <div className="bg-cyan-400/20 dark:bg-slate-700/50 my-3 p-4 rounded">
         {/* <pre>{JSON.stringify(query, null, 2)}</pre> */}
-        <div className="text-cyan-400">{query.data as string}</div>
+
+        <div className="text-cyan-600 dark:text-cyan-600 ">
+          {query.data as string}
+        </div>
+        {/* params */}
+        <div className="m-1">
+          <Collapsible>
+            <CollapsibleTrigger>Inference Parameters</CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="text-sm font-mono">
+                {JSON.stringify(
+                  { model, temperature, repeat_penalty, top_k, top_p },
+                  null,
+                  2,
+                )}
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </div>
       </div>
-      <div>
+      <div className=" my-3">
         {" "}
         <Button
           variant="ghost"
@@ -47,7 +70,7 @@ export default function IterationResult(props: IProps) {
             })
           }
         >
-          <ReloadIcon className="h-4 w-4 text-cyan-400" />
+          <ReloadIcon className="h-4 w-4 text-cyan-600 dark:text-cyan-600 " />
         </Button>
       </div>
     </div>
