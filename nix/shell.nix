5cac8373271021c48255b01be3b775a16648687d
sntx <sntx.htqjd@simplelogin.com>
2024-08-02 11:04:35 +0200

    feat: added nix tooling

---
+++ nix/shell.nix
@@ -0,0 +1,23 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  inherit (pkgs) lib;
+in
+pkgs.mkShell rec {
+  CARGO_PROFILE_RELEASE_DEBUG = true;
+  LD_LIBRARY_PATH = lib.makeLibraryPath nativeBuildInputs;
+  RUST_BACKTRACE = "full";
+
+  nativeBuildInputs = with pkgs; [
+    bun
+    cargo
+    cargo
+    gcc
+    gnome.libsoup
+    nixfmt-rfc-style
+    pkg-config
+    rustc
+    webkitgtk
+  ];
+}
