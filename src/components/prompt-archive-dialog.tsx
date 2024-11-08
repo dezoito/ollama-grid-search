b716384d6fe15b2ac86a7b106d58b31df6f68310
dezoito <an.dezoito@gmail.com>
2024-11-08 19:34:04 -0300

    feature/sql: modal margins

--- src/components/prompt-archive-dialog.tsx
+++ src/components/prompt-archive-dialog.tsx
@@ -1,16 +1,13 @@
-import { ArchiveIcon } from "@radix-ui/react-icons";
-
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import { ArchiveIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 
 export function PromptArchiveDialog() {
@@ -23,18 +20,22 @@ export function PromptArchiveDialog() {
           <ArchiveIcon className="h-5 w-5" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Prompt Archive</DialogTitle>
-          <DialogDescription>Prompt archive desc</DialogDescription>
-        </DialogHeader>
-        {/* 
-            Content here
-             */}
+      <DialogContent className="fixed left-2.5 right-2.5 top-5 h-[calc(100vh-30px)] w-[calc(100vw-20px)] max-w-none translate-x-0 translate-y-0">
+        <div className="flex h-full flex-col">
+          {/* Header */}
+          <DialogHeader className="border-b px-6 py-4 dark:border-gray-800">
+            <DialogTitle>Prompt Archive</DialogTitle>
+            <DialogDescription>Prompt archive desc</DialogDescription>
+          </DialogHeader>
 
-        {/* we need to set a max-height on this div for overflow to work */}
-        <div className="grid max-h-[500px] gap-6 overflow-y-auto px-4 py-4"></div>
-        <DialogFooter>Footer Content</DialogFooter>
+          {/* Main Content Area */}
+          <div className="flex-1 overflow-y-auto p-6">{/* Content here */}</div>
+
+          {/* Footer */}
+          {/* <DialogFooter className="border-t p-6 dark:border-gray-800">
+            Footer Content
+          </DialogFooter> */}
+        </div>
       </DialogContent>
     </Dialog>
   );
